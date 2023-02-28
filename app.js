de983bd2de4e9b829f6efdf7960fbd778244f828
Kunwar8 <121372326+Kunwar8@users.noreply.github.com>
2023-02-28 14:16:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ function App() {
 }
 
 export default App;
+
+
+// to render the time stamp along with the Hello World message
+function App(props) {
+  const currDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+export default App;
